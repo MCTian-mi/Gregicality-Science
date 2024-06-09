cc1a47446a67e571001ea3549dd9391c5d940634
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2024-06-09 22:57:16 +0800

    backup

--- src/main/java/gregicality/science/api/capability/IPressureMachine.java
+++ src/main/java/gregicality/science/api/capability/IPressureMachine.java
@@ -3,6 +3,7 @@ package gregicality.science.api.capability;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
@@ -19,6 +20,7 @@ public interface IPressureMachine {
     BlockPos getPos();
 
     default void balancePressure() {
+        double oldPressure = getPressureContainer().getPressure();
         ArrayList<IPressureContainer> containers = new ArrayList<>();
         for (EnumFacing facing : EnumFacing.VALUES) {
             TileEntity tile = getWorld().getTileEntity(getPos().offset(facing));
@@ -32,7 +34,35 @@ public interface IPressureMachine {
         if (containers.isEmpty()) return;
         containers.add(this.getPressureContainer());
         IPressureContainer.mergeContainers(false, containers.toArray(new IPressureContainer[0]));
-        if (!getPressureContainer().isPressureSafe())
+
+        double newPressure = getPressureContainer().getPressure();
+        if (!getPressureContainer().isPressureSafe(newPressure)) {
             getPressureContainer().causePressureExplosion(getWorld(), getPos()); // TODO
+        }
+        adjustTickRate(oldPressure, newPressure);
+    }
+
+    void setTickRate(int rate);
+
+    int getTickRate();
+
+    default int getDefaultTickRate() {
+        return 20;
+    }
+
+    default int getMaxTickRate() {
+        return 2;
+    }
+
+    default int getMinTickRate() {
+        return 100;
+    }
+
+    default void adjustTickRate(double oldPressure, double newPressure) {
+        if (newPressure > 2 * oldPressure || newPressure < 0.5 * oldPressure) {
+            setTickRate(MathHelper.clamp(getTickRate() * 2, getMinTickRate(), getMaxTickRate()));
+        } else {
+            setTickRate(MathHelper.clamp(getTickRate() / 2, getMinTickRate(), getMaxTickRate()));
+        }
     }
 }
